a0d15deb04e5ff905944629541a032c43e1298ad
Thomas Proffen <thomasproffen@gmail.com>
2019-02-21 21:56:56 -0500

    Create setup_activity2.sh

---
+++ TextMining/Helpers/setup_activity2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Copy all needed files
+
+echo "Fetching needed files .."
+mkdir -p Helpers
+curl -s -o Helpers/load_data.py https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Helpers/load_data.py
+curl -s -o Helpers/plot_data.py https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Helpers/plot_data.py
+mkdir -p Data
+curl -s -o Data/01_philosophers_stone.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Helpers/01_philosophers_stone.txt
+curl -s -o Data/02_chamber_of_secrets.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Helpers/02_chamber_of_secrets.txt
+curl -s -o Data/03_prisoner_of_azkaban.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Helpers/03_prisoner_of_azkaban.txt
+curl -s -o Data/04_goblet_of_fire.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Helpers/04_goblet_of_fire.txt
+curl -s -o Data/05_order_of_the_phoenix.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Helpers/05_order_of_the_phoenix.txt
+curl -s -o Data/06_half_blood_prince.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Helpers/06_half_blood_prince.txt
+curl -s -o Data/07_deathly_hallows.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Helpers/07_deathly_hallows.txt
+echo "Done"
