b01d5dee791e97241a5cafdd335917828524874f
Thomas Proffen <thomasproffen@gmail.com>
2019-02-21 21:08:57 -0500

    Update setup.sh

--- Algorithms/Helpers/setup.sh
+++ Algorithms/Helpers/setup.sh
@@ -2,7 +2,7 @@
 # Copy all needed files
 
 echo "Fetching needed files .."
-mkdir -P Helpers
+mkdir -p Helpers
 curl -s -o Helpers/helpers.py https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/Algorithms/Helpers/helpers.py
 
 echo "Done"
