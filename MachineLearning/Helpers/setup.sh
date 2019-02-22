612ae796ee3b46d32dfcca2e59f5fb67e3e197e8
Thomas Proffen <thomasproffen@gmail.com>
2019-02-21 21:19:25 -0500

    Update setup.sh

--- MachineLearning/Helpers/setup.sh
+++ MachineLearning/Helpers/setup.sh
@@ -2,7 +2,9 @@
 # Copy all needed files
 
 echo "Fetching needed files .."
+mkdir -p Helpers
 mkdir -p Data
+curl -o Helpers/helpers.py https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/MachineLearning/Helpers/helpers.py
 curl -o Data/Fruit.csv https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/MachineLearning/Data/Fruit.csv
 
 echo "Done"
