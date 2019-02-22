3c174e18472054d1669e0b497765d970b57cb0e1
Thomas Proffen <thomasproffen@gmail.com>
2019-02-21 21:20:32 -0500

    Update setup.sh

--- MachineLearning/Helpers/setup.sh
+++ MachineLearning/Helpers/setup.sh
@@ -6,5 +6,6 @@ mkdir -p Helpers
 mkdir -p Data
 curl -o Helpers/helpers.py https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/MachineLearning/Helpers/helpers.py
 curl -o Data/Fruit.csv https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/MachineLearning/Data/Fruit.csv
+curl -o Data/Cats.csv https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/MachineLearning/Data/Cats.csv
 
 echo "Done"
