ab44a9f3cf770385b0005131e6e1af7885caaff1
Thomas Proffen <thomasproffen@gmail.com>
2020-01-24 11:59:45 -0500

    Fixed data destination directory creation

--- TextMining/Helpers/setup_activity5.sh
+++ TextMining/Helpers/setup_activity5.sh
@@ -6,7 +6,7 @@ mkdir -p Helpers
 curl -s -o Helpers/load_movies.py https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Helpers/load_movies.py
 curl -s -o Helpers/plot_data.py https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Helpers/plot_data.py
 curl -s -o Helpers/clean_data.py https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Helpers/clean_data.py
-mkdir -p Data
+mkdir -p Data/Bonus
 curl -s -o Data/Bonus/01_phantom_menace.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Data/Bonus/01_phantom_menace.txt
 curl -s -o Data/Bonus/02_attack_of_the_clones.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Data/Bonus/02_attack_of_the_clones.txt
 curl -s -o Data/Bonus/03_revenge_of_the_sith.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Data/Bonus/03_revenge_of_the_sith.txt
