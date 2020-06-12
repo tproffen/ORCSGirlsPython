a20f9c8951fabdd63cbedba2bab9e14bd60709a0
Thomas Proffen <thomasproffen@gmail.com>
2020-06-12 11:09:01 -0400

    Fixing setup scripts

--- TextMining/Helpers/setup_activity5.sh
+++ TextMining/Helpers/setup_activity5.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 # Copy all needed files
 
+#!/bin/bash
+
+# Install needed Python Packages
+
+!pip install nltk
+!pip install vaderSentiment
+#!python -m nltk.downloader all
+
+# Copy all needed files
+
 echo "Fetching needed files .."
 mkdir -p Helpers
-curl -s -o Helpers/load_movies.py https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Helpers/load_movies.py
+curl -s -o Helpers/load_data.py https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Helpers/load_data.py
 curl -s -o Helpers/plot_data.py https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Helpers/plot_data.py
 curl -s -o Helpers/clean_data.py https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Helpers/clean_data.py
-mkdir -p Data/Bonus
-curl -s -o Data/Bonus/01_phantom_menace.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Data/Bonus/01_phantom_menace.txt
-curl -s -o Data/Bonus/02_attack_of_the_clones.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Data/Bonus/02_attack_of_the_clones.txt
-curl -s -o Data/Bonus/03_revenge_of_the_sith.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Data/Bonus/03_revenge_of_the_sith.txt
-curl -s -o Data/Bonus/04_a_new_hope.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Data/Bonus/04_a_new_hope.txt
-curl -s -o Data/Bonus/05_the_empire_strikes_back.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Data/Bonus/05_the_empire_strikes_back.txt
-curl -s -o Data/Bonus/06_return_of_the_jedi.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Data/Bonus/06_return_of_the_jedi.txt
-curl -s -o Data/Bonus/07_the_force_awakens.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Data/Bonus/07_the_force_awakens.txt
+mkdir -p Data
+curl -s -o Data/01_philosophers_stone.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Data/01_philosophers_stone.txt
+curl -s -o Data/02_chamber_of_secrets.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Data/02_chamber_of_secrets.txt
+curl -s -o Data/03_prisoner_of_azkaban.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Data/03_prisoner_of_azkaban.txt
+curl -s -o Data/04_goblet_of_fire.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Data/04_goblet_of_fire.txt
+curl -s -o Data/05_order_of_the_phoenix.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Data/05_order_of_the_phoenix.txt
+curl -s -o Data/06_half_blood_prince.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Data/06_half_blood_prince.txt
+curl -s -o Data/07_deathly_hallows.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Data/07_deathly_hallows.txt
 curl -s -o Data/stopwords.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Data/stopwords.txt
 echo "Done"
---
+++ TextMining/Helpers/setup_activity6.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Copy all needed files
+
+echo "Fetching needed files .."
+mkdir -p Helpers
+curl -s -o Helpers/load_movies.py https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Helpers/load_movies.py
+curl -s -o Helpers/plot_data.py https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Helpers/plot_data.py
+curl -s -o Helpers/clean_data.py https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Helpers/clean_data.py
+mkdir -p Data/Bonus
+curl -s -o Data/Bonus/01_phantom_menace.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Data/Bonus/01_phantom_menace.txt
+curl -s -o Data/Bonus/02_attack_of_the_clones.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Data/Bonus/02_attack_of_the_clones.txt
+curl -s -o Data/Bonus/03_revenge_of_the_sith.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Data/Bonus/03_revenge_of_the_sith.txt
+curl -s -o Data/Bonus/04_a_new_hope.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Data/Bonus/04_a_new_hope.txt
+curl -s -o Data/Bonus/05_the_empire_strikes_back.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Data/Bonus/05_the_empire_strikes_back.txt
+curl -s -o Data/Bonus/06_return_of_the_jedi.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Data/Bonus/06_return_of_the_jedi.txt
+curl -s -o Data/Bonus/07_the_force_awakens.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Data/Bonus/07_the_force_awakens.txt
+curl -s -o Data/stopwords.txt https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/TextMining/Data/stopwords.txt
+echo "Done"
