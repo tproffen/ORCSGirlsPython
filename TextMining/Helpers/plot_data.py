b9d5d43badd55a2047174b593bf28b160e3b3a46
Herrmannova, Dasha <herrmannovad@ornl.gov>
2020-06-11 23:56:53 -0400

    Updated helpers

--- TextMining/Helpers/plot_data.py
+++ TextMining/Helpers/plot_data.py
@@ -42,3 +42,37 @@ def plot_words(data):
     plt.xticks(positions, words, rotation=45)
 
     plt.show()
+    
+def plot_book_scores(chapter_names, chapter_scores):
+    
+    plt.figure(figsize=(12, 8))
+    
+    pos = list(range(len(chapter_scores)))
+    
+    plt.plot(pos, list(chapter_scores))
+    
+    plt.xticks(pos, chapter_names, rotation=90)
+    
+    plt.show()
+
+def plot_all_book_scores(all_names, all_scores):
+    plt.figure(figsize=(20, 10))
+    
+    c = ['blue', 'green', 'yellow', 'red', 'black', 'orange', 'purple']
+    
+    previous = 0
+
+    for idx, scores in enumerate(all_scores):
+        pos = [i + previous for i in range(len(scores))]
+        previous += len(scores)
+        plt.plot(pos, scores, color=c[idx])
+
+    plt.xlim((-0.5, previous + 0.5))
+    xticks = [n for ns in all_names for n in ns]
+    plt.xticks(list(range(len(xticks))), xticks, rotation=90, fontsize=6)
+    
+    plt.tight_layout()
+    
+    plt.savefig('all_chapters.pdf')
+    
+    plt.show()
