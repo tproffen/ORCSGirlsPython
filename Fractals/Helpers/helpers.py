18198d6ebfc3662279fdae9b9dd2e1535d5140e8
Thomas Proffen <thomasproffen@gmail.com>
2021-02-13 15:52:31 -0500

    Moved fractal routines to proper class

--- Fractals/Helpers/helpers.py
+++ Fractals/Helpers/helpers.py
@@ -3,3 +3,27 @@ from numpy import arange
 import math
 
 #------------------------------------------------------------------------
+def koch(size, order):
+
+    if order > 0:
+        for t in [60, -120, 60, 0]:
+            koch(size/3, order-1)
+            left(t)
+    else:
+      pendown()
+      forward(round(size))
+
+#------------------------------------------------------------------------
+def snowflake(x_start, y_start, size, order):
+
+  # goto center
+  penup()
+  goto(x_start, y_start)
+  backward(round(size/1.732))
+  left(30)
+  pendown()
+
+  # Three Koch curves
+  for i in range(3):
+      koch(size, order)
+      right(120)
