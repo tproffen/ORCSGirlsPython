69ad936f9e9939b644ffceac736fc60548e3c384
Thomas Proffen <thomasproffen@gmail.com>
2021-02-17 11:57:17 -0500

    Update turtleCanvas.py

--- ArtisticMath/Helpers/turtleCanvas.py
+++ ArtisticMath/Helpers/turtleCanvas.py
@@ -305,7 +305,7 @@ def liveoff():
 def speed(speed):
   global currentCanvas;
   if (speed > 0):
-    currentCanvas['delay']=round(1000./speed)
+    currentCanvas['delay']=min(round(1000./speed),10)
   else:
     currentCanvas['delay']=0
 
