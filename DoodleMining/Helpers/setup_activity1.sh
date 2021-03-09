6ed0532e9d022baa134389c75b0605a6c8e4789a
Thomas Proffen <thomasproffen@gmail.com>
2021-03-09 12:36:40 -0500

    Update setup_activity1.sh

--- DoodleMining/Helpers/setup_activity1.sh
+++ DoodleMining/Helpers/setup_activity1.sh
@@ -9,4 +9,9 @@ curl -s -o Helpers/helpers.py https://raw.githubusercontent.com/tproffen/ORCSGir
 curl -s -o Helpers/turtle.py https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/ArtisticMath/Helpers/turtle.py
 curl -s -o Helpers/turtleCanvas.py https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/ArtisticMath/Helpers/turtleCanvas.py
 
+# Getting updated QuickDraw Python API (for now)
+mkdir -p Helpers/quickdraw
+curl -s -o Helpers/quickdraw/data.py https://raw.githubusercontent.com/tproffen/quickdraw_python/fix-for-Python-3.7/quickdraw/data.py
+curl -s -o Helpers/quickdraw/names.py https://raw.githubusercontent.com/tproffen/quickdraw_python/fix-for-Python-3.7/quickdraw/names.py
+
 echo "Done"
