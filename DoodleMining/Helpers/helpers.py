72cf0b3d16d560319a7d27c821713e64ed4af3c7
Thomas Proffen <thomasproffen@gmail.com>
2021-03-18 12:01:56 -0400

    Update helpers.py

--- DoodleMining/Helpers/helpers.py
+++ DoodleMining/Helpers/helpers.py
@@ -8,3 +8,19 @@ import random
 import math
 
 #------------------------------------------------------------------------
+# Some plotting helpers
+#------------------------------------------------------------------------
+
+def plot_image(img):
+  plt.figure()
+  plt.imshow(image)
+  plt.axis("off")
+  plt.show()
+
+def plot(numbers, labels):
+  plt.figure(figsize=(12, 8))
+    
+  pos = list(range(len(numbers)))
+  plt.bar(pos, numbers)
+  plt.xticks(pos, list(labels), rotation=90)    
+  plt.show()
