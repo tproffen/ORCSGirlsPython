57731774c0d780062c9256c6173d83b702d1fbfa
Thomas Proffen <thomasproffen@gmail.com>
2022-03-26 12:16:52 -0400

    Update setup_activities.sh

--- ComputerArt/Helpers/setup_activities.sh
+++ ComputerArt/Helpers/setup_activities.sh
@@ -4,6 +4,7 @@
 echo "Fetching needed files .."
 mkdir -p Helpers
 curl -s -o Helpers/helpers.py https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/ComputerArt/Helpers/helpers.py
+curl -s -o Helpers/fractals.py https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/Fractals/Helpers/helpers.py
 curl -s -o Helpers/turtle.py https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/ComputerArt/Helpers/turtle.py
 curl -s -o Helpers/turtleCanvas.py https://raw.githubusercontent.com/tproffen/ORCSGirlsPython/master/ComputerArt/Helpers/turtleCanvas.py
 
