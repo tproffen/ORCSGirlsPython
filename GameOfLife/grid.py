8c0e712d590637e08210a14eeba4f8b42f15b3ef
Thomas Proffen <thomasproffen@gmail.com>
2024-03-29 14:15:42 -0400

    Indent fixes

--- GameOfLife/grid.py
+++ GameOfLife/grid.py
@@ -3,36 +3,35 @@ from PIL import Image
 from numpy import asarray
 
 class Grid:
-	def __init__(self, width, height, cell_size):
-		self.rows = height // cell_size
-		self.columns = width // cell_size
-		self.cell_size = cell_size
-		self.cells = [[0 for _ in range(self.columns)] for _ in range(self.rows)]
+    def __init__(self, width, height, cell_size):
+        self.rows = height // cell_size
+        self.columns = width // cell_size
+        self.cell_size = cell_size
+        self.cells = [[0 for _ in range(self.columns)] for _ in range(self.rows)]
 
-	def draw(self, window):
-		for row in range(self.rows):
-			for column in range(self.columns):
-				color = (0, 255, 0) if self.cells[row][column] else  (55, 55, 55)
-				pygame.draw.rect(window, color, (column * self.cell_size, row * self.cell_size, self.cell_size -1, self.cell_size - 1))
+    def draw(self, window):
+        for row in range(self.rows):
+            for column in range(self.columns):
+                color = (0, 255, 0) if self.cells[row][column] else (55, 55, 55)
+                pygame.draw.rect(window, color, (column * self.cell_size, row * self.cell_size, self.cell_size -1, self.cell_size - 1))
 
-	def fill_random(self):
-		for row in range(self.rows):
-			for column in range(self.columns):
-				self.cells[row][column] = random.choice([1, 0, 0, 0])
+    def fill_random(self):
+        self.cells = [[random.choice([1, 0, 0, 0]) for _ in range(self.columns)] for _ in range(self.rows)]
                 
-	def fill_image(self, file_name):
-		print(f"Reading image from {file_name} ..")
-		image = Image.open(file_name).resize((self.columns, self.rows))
-		data = asarray(image.convert("L"))
-		for row in range(self.rows):
-			for column in range(self.columns):
-				self.cells[row][column] = 1 if data[row][column]==255 else 0
+    def fill_image(self, file_name):
+        print(f"Reading image from {file_name} ..")
+        image = Image.open(file_name).resize((self.columns, self.rows))
+        data = asarray(image.convert("L"))
+        for row in range(self.rows):
+            for column in range(self.columns):
+                self.cells[row][column] = 1 if data[row][column]==255 else 0
         
-	def clear(self):
-		for row in range(self.rows):
-			for column in range(self.columns):
-				self.cells[row][column] = 0
+    def clear(self):
+        for row in range(self.rows):
+            for column in range(self.columns):
+                self.cells[row][column] = 0
 
-	def toggle_cell(self, row, column):
-		if 0 <= row < self.rows and 0 <= column < self.columns:
-			self.cells[row][column] = not self.cells[row][column]
+    def toggle_cell(self, row, column):
+        if 0 <= row < self.rows and 0 <= column < self.columns:
+            self.cells[row][column] = not self.cells[row][column]
+            
--- GameOfLife/main.py
+++ GameOfLife/main.py
@@ -18,41 +18,41 @@ simulation = Simulation(WINDOW_WIDTH, WINDOW_HEIGHT, CELL_SIZE)
 #Simulation Loop
 while True:
 
-	# 1. Event Handling
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			sys.exit()
-		if event.type == pygame.MOUSEBUTTONDOWN:
-			pos = pygame.mouse.get_pos()
-			row = pos[1] // CELL_SIZE
-			column = pos[0] // CELL_SIZE
-			simulation.toggle_cell(row, column)
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_RETURN:
-				simulation.start()
-				pygame.display.set_caption("Game of Life is running")
-			elif event.key == pygame.K_SPACE:
-				simulation.stop()
-				pygame.display.set_caption("Game of Life has stopped")
-			elif event.key == pygame.K_f:
-				FPS += 2
-			elif event.key == pygame.K_s:
-				if FPS > 5:
-					FPS -= 2
-			elif event.key == pygame.K_r:
-				simulation.create_random_state()
-			elif event.key == pygame.K_c:
-				simulation.clear()
-			elif event.key == pygame.K_i:
-				simulation.create_from_image('image.png')
+    # 1. Event Handling
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            row = pos[1] // CELL_SIZE
+            column = pos[0] // CELL_SIZE
+            simulation.toggle_cell(row, column)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                simulation.start()
+                pygame.display.set_caption("Game of Life is running")
+            elif event.key == pygame.K_SPACE:
+                simulation.stop()
+                pygame.display.set_caption("Game of Life has stopped")
+            elif event.key == pygame.K_f:
+                FPS += 2
+            elif event.key == pygame.K_s:
+                if FPS > 5:
+                    FPS -= 2
+            elif event.key == pygame.K_r:
+                simulation.create_random_state()
+            elif event.key == pygame.K_c:
+                simulation.clear()
+            elif event.key == pygame.K_i:
+                simulation.create_from_image('image.png')
 
-	# 2. Updating State
-	simulation.update()
+    # 2. Updating State
+    simulation.update()
 
-	# 3. Drawing
-	window.fill(GREY)
-	simulation.draw(window)
+    # 3. Drawing
+    window.fill(GREY)
+    simulation.draw(window)
 
-	pygame.display.update()
-	clock.tick(FPS)
+    pygame.display.update()
+    clock.tick(FPS)
